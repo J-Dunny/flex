9cda9c14306eda65ce5184afc60b76aa8a50b0e7
Jake Dunafon <jlad795@gmail.com>
2022-04-22 09:04:58 -0700

    Add delete function

--- src/Workout/Workout.js
+++ src/Workout/Workout.js
@@ -2,18 +2,18 @@ import React from "react"
 import Exercise from "../Exercise/Exercise"
 import './Workout.css'
 
-const Workout = ({workout}) => {
+const Workout = ({ workout, deleteWorkout }) => {
     let exercises;
-    
-    if(workout){
+
+    if (workout) {
         exercises = workout.exercises.map(exercise => {
-            
-            return <Exercise key={exercise.id} exercise={exercise}/>
+            return <Exercise key={exercise.id} exercise={exercise} />
         })
     }
-    
+
     return (
         <section className="workout">
+            <button onClick={() => deleteWorkout(workout.id)}>X</button>
             <h1>{workout.title}</h1>
             {exercises}
             <button>Do it!</button>
