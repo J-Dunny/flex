da5b39c1376a30cb26ee66b901f81331d93f4bbb
Jake Dunafon <jlad795@gmail.com>
2022-04-22 09:03:28 -0700

    Add delete function

--- src/AllWorkouts/AllWorkouts.js
+++ src/AllWorkouts/AllWorkouts.js
@@ -3,22 +3,20 @@ import Workout from "../Workout/Workout"
 import { NavLink } from "react-router-dom"
 import './AllWorkouts.css'
 
-const AllWorkouts = ({ workouts }) => {
+const AllWorkouts = ({ workouts, deleteWorkout }) => {
     let workoutCards;
-    console.log("workouts", workouts)
+
     if (workouts[0]) {
         workoutCards = workouts.map(workout => {
-            return <Workout key={workout} workout={workout} />
+            return <Workout key={workout} workout={workout} deleteWorkout={deleteWorkout} />
         })
     }
 
     return (
         <div>
             <NavLink to="/addworkout"><button>Create Workout</button></NavLink>
-            <h2>All your workouts</h2>
-
+            {workouts[0] ? <h2>All your workouts</h2> : <h2>You haven't created any workouts yet. Click Create Workout to begin!</h2>}
             <section className="all-workouts">
-
                 {workoutCards}
             </section>
         </div>
