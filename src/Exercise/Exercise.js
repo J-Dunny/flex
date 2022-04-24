ef2b272d48d2a928825fbcdc6fab076b806f49df
Jake Dunafon <jlad795@gmail.com>
2022-04-24 14:38:55 -0700

    Update excersize to heroku

--- src/Exercise/Exercise.js
+++ src/Exercise/Exercise.js
@@ -31,7 +31,7 @@ class Exercise extends Component {
     }
 
     render() {
-        const doWorkoutPage = `http://localhost:3000/doworkout/${this.props.id}`
+        const doWorkoutPage = `https://flex-react-app.herokuapp.com/doworkout/${this.props.id}`
         // console.log("win",window.location.href)
         // console.log("do",doWorkoutPage)
         let allSets;
