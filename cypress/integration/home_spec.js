232184896846416f975457fb5f1f241e1f427d92
Jake Dunafon <jlad795@gmail.com>
2022-04-24 17:58:28 -0700

    Add random test

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,50 +1,49 @@
 describe('Home page flow', () => {
     beforeEach(() => {
-      cy.intercept("GET", "https://exercisedb.p.rapidapi.com/exercises", {
-        newExercises: [
-            {
-                bodyPart: "waist",
-                equipment: "body weight",
-                gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0001.gif",
-                id: "0001",
-                name: "3/4 sit-up",
-                target: "abs"
-            },
-              {
-                bodyPart: "waist",
-                equipment: "body weight",
-                gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0002.gif",
-                id: "0002",
-                name: "45° side bend",
-                target: "abs"
-              },
-              {
-                bodyPart: "upper legs",
-                equipment: "body weight",
-                gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1512.gif",
-                id: "1512",
-                name: "all fours squad stretch",
-                target: "quads"
-              },
-              {
-                bodyPart: "back",
-                equipment: "cable",
-                gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
-                id: "0007",
-                name: "alternate lateral pulldown",
-                target: "lats"
-              },
-              {
-                bodyPart: "upper legs",
-                equipment: "assisted",
-                gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1710.gif",
-                id: "1710",
-                name: "assisted lying gluteus and piriformis stretch",
-                target: "glutes"
-              }
-
-          ]
-        }).as('get-exercises')
+      cy.intercept("GET", "https://exercisedb.p.rapidapi.com/exercises", 
+      [
+        {
+            bodyPart: "waist",
+            equipment: "body weight",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0001.gif",
+            id: "0001",
+            name: "3/4 sit-up",
+            target: "abs"
+        },
+          {
+            bodyPart: "waist",
+            equipment: "body weight",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0002.gif",
+            id: "0002",
+            name: "45° side bend",
+            target: "abs"
+          },
+          {
+            bodyPart: "upper legs",
+            equipment: "body weight",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1512.gif",
+            id: "1512",
+            name: "all fours squad stretch",
+            target: "quads"
+          },
+          {
+            bodyPart: "back",
+            equipment: "cable",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+            id: "0007",
+            name: "alternate lateral pulldown",
+            target: "lats"
+          },
+          {
+            bodyPart: "upper legs",
+            equipment: "assisted",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1710.gif",
+            id: "1710",
+            name: "assisted lying gluteus and piriformis stretch",
+            target: "glutes"
+          }
+        ]
+      ).as('get-exercises')
 
 
     })
@@ -69,12 +68,6 @@ describe('Home page flow', () => {
         .contains("You haven't created any workouts yet. Click Create Workout to begin!")
     })
 
-    it('Should generate random workout ', () => {
-        // cy.visit('http://localhost:3000/home')
-        // .get('button')
-        // .contains('Generate Random Workout').click()
-        // I was unable to test this in cypress do to the fact that the function in my App.js is creating random numbers up to 1327 and I only have 5 exercises in my stubbed array...
-    })
 
     it('Should navigate to create workout page ', () => {
         cy.visit('http://localhost:3000/home')
@@ -83,8 +76,17 @@ describe('Home page flow', () => {
         .get("h1")
         .contains('Create a new Workout!')
     })
-    
 
+    it('Should generate random workout ', () => {
+        cy.visit('http://localhost:3000/home')
+        .get('button').contains('Generate Random Workout').click()
+        .get('h1').contains('Random Workout')
+        .get('button').contains('❌')
+        .get('button').contains('FLEX!')
+        // I was unable to leave this test in cypress do to the fact that the function in my App.js is creating random numbers up to 1327 and I only have 5 exercises in my stubbed array...
+
+        //I was able to get the test to work when I changed my function to only make random exercises with indexes 0-4
+    })
 
 })
 
